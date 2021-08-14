5f48f22d9a59ff1f58fe22204b7d2b5fa44ec302
Alexander Rovang <wolf_vii@yahoo.com>
2021-08-14 17:01:12 -0400

    sets up store, imports necessary dep in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import memberReducer from './reducers/memberReducer'
+import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/style.css';
 import reportWebVitals from './reportWebVitals';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(memberReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router basename={process.env.PUBLIC_URL}>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
