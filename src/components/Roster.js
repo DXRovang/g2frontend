b48641d07a5665eac097731c75b3ad538d3b1bd0
Alexander Rovang <wolf_vii@yahoo.com>
2021-10-15 13:55:14 -0500

    adds title and additional text

--- src/components/Roster.js
+++ src/components/Roster.js
@@ -6,6 +6,7 @@ import {
 import { connect } from 'react-redux'
 import { addVote } from '../actions/addVote'
 import thumb from '../assets/thumb.svg'
+import Title from '../components/Title'
 
 
 class Roster extends React.Component{
@@ -17,7 +18,8 @@ handleVote = (member) =>{
 render(){
   const fullList = this.props.members
   return(
-
+    <>
+    <Title ClassName="title"/>
     <CardGroup className="group">
        <ul>
        {fullList ? fullList.map(member =>
@@ -29,15 +31,19 @@ render(){
               <CardTitle tag="h5">{member.name}</CardTitle>
               <CardSubtitle tag="h6" className="mb-2 text-muted">{member.title}</CardSubtitle>
               <CardText>{member.bio}</CardText>
+              <div>
+                <CardText className="left">Want to work with {member.name}?  </CardText>
+                <button className="left" onClick={() => this.handleVote(member)}><CardImg className="icon"src={thumb}/></button>
+                <CardText className="left">yes!</CardText>
+              </div>
               {member.votes ? 
-              <CardText>{member.votes} vote(s) so far!</CardText>: null}
-              <button  onClick={() => this.handleVote(member)}><CardImg className="icon"src={thumb}/></button>
+              <CardText className="new">{member.votes} people have said Yes!</CardText>: null}
             </CardBody>
           </Card>
           </li>): null}
       </ul>
     </CardGroup>
-
+  </>
   )
 }
 }
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = props =>{
+
+    
+    return(
+      <div>G2 Crowd Team Roster!</div>
+    )
+  }
+
+  export default Title
